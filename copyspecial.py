81e8e966754ece0e191d9d23b08557f958e0a16b
Jessica Woods <jessica_woods53@yahoo.com>
2020-07-05 18:54:59 -0400

    almost done

--- copyspecial.py
+++ copyspecial.py
@@ -7,7 +7,7 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 
 # give credits
-__author__ = "???"
+__author__ = "Jessica and Friends"
 
 import re
 import os
@@ -19,17 +19,26 @@ import argparse
 
 def get_special_paths(dirname):
     """Given a dirname, returns a list of all its special files."""
-    # your code here
-    return
+    paths = os.listdir(dirname)
+    special_paths = []
+    for filename in paths:
+        match = re.search(r'__\w+__', filename)
+        if match:
+            special_paths.append(os.path.abspath(
+                os.path.join(dirname, filename)))
+    return special_paths
 
 
 def copy_to(path_list, dest_dir):
-    # your code here
-    return
 
 
 def zip_to(path_list, dest_zip):
-    # your code here
+    cmd = ['zip', '-j', dest_zip]
+    cmd.extend(path_list)
+    print("Command I'm going to do:")
+    print(' '.join(cmd))
+    subprocess.check_output(cmd)
+
     return
 
 
@@ -39,7 +48,7 @@ def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
-    # TODO: add one more argument definition to parse the 'from_dir' argument
+    parser.add_argument('from_dir', help='dest zipfile for special files')
     ns = parser.parse_args(args)
 
     # TODO: you must write your own code to get the command line args.
@@ -49,8 +58,12 @@ def main(args):
     # This is input data validation. If something is wrong (or missing) with
     # any required args, the general rule is to print a usage message and
     # exit(1).
+    special_paths = get_special_paths(ns.from_dir)
 
-    # Your code here: Invoke (call) your functions
+    if ns.tozip:
+        zip_to(special_paths, ns.tozip)
+    else:
+        print('\n'.join(special_paths))
 
 
 if __name__ == "__main__":
